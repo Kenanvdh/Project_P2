2e8f2413beef2b90ccf79ab0676b683f6253331e
Kenanvdh <kenanvdh@ziggo.nl>
2023-11-16 16:28:23 +0100

    Footer addedd

---
+++ libs/songlist/ui/src/lib/footer/footer.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'indivproj-p2-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: [],
+})
+export class FooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
--- libs/songlist/ui/src/lib/ui.module.ts
+++ libs/songlist/ui/src/lib/ui.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavComponent } from './nav/nav.component';
 import { RouterLink } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [CommonModule, RouterLink],
-  declarations: [NavComponent],
-  exports: [NavComponent],
+  declarations: [NavComponent, FooterComponent],
+  exports: [NavComponent, FooterComponent],
 })
 export class UiModule {}
