7d43b216750c89e5456d4b7e805cf99e416131a1
Kenanvdh <kenanvdh@ziggo.nl>
2023-11-17 15:26:56 +0100

    minor changes

--- libs/songlist/features/src/lib/features.module.ts
+++ libs/songlist/features/src/lib/features.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AboutComponent } from './about/about.component';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule, RouterLink, Routes } from '@angular/router';
+import { RouterModule, RouterLink } from '@angular/router';
 import { SongListComponent } from './song/song-list/song-list.component';
 import { SongDetailComponent } from './song/song-detail/song-detail.component';
 import { UserDetail } from './user/user-detail/user-detail.component';
