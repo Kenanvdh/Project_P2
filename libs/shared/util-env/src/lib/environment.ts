eda576922cdb1e2f8bc01eb3cbf397c0e2b539cf
Kenanvdh <kenanvdh@ziggo.nl>
2023-11-17 12:29:08 +0100

    commented all code environment.ts

--- libs/shared/util-env/src/lib/environment.ts
+++ libs/shared/util-env/src/lib/environment.ts
@@ -1,6 +1,6 @@
-import { IEnvironment } from './enivronment.interface';
+// import { IEnvironment } from './enivronment.interface';
 
-export const environment: IEnvironment = {
-  production: false,
-  backendUrl: 'http://localhost:3000/api',
-};
+// export const environment: IEnvironment = {
+//   production: false,
+//   backendUrl: 'http://localhost:3000/api',
+// };
