b0be3cf94bc8b4899b8ec7756f59437be9e879a3
Kenanvdh <kenanvdh@ziggo.nl>
2023-11-17 18:05:35 +0100

    route change song crud

--- apps/song-list-web/src/app/app.routes.ts
+++ apps/song-list-web/src/app/app.routes.ts
@@ -14,14 +14,14 @@ export const routes: Routes = [
     { path: "songlist", component: SongListComponent },
     { path: "song/create", component: SongEditComponent },
     { path: "songlist/:id", component: SongEditComponent },
-    { path: "song/:id/edit", component: SongDetailComponent },
-    { path: "song/:id/delete", component: SongDeleteComponent },
     { path: "about", pathMatch: "full", component: AboutComponent },
     { path: "users", pathMatch: "full", component: ListComponent },
     { path: "user/create", pathMatch: "full", component: UserEditComponent },
     { path: "users/:id", pathMatch: "full", component: UserDetail },
     { path: "users/:id/edit", pathMatch: "full", component: UserEditComponent },
     { path: "users/:id/delete", pathMatch: "full", component: UserDeleteComponent },
+    { path: "song/:id/edit", pathMatch: "full", component: SongDetailComponent },
+    { path: "song/:id/delete", pathMatch: "full",component: SongDeleteComponent },
     { path: "**", redirectTo: "songlist" },
 ];
 
